771ea997bf0167f5c1f315da395af74f92e7a371
Simon <Simon.Jonsson@molnlycke.com>
2026-06-12 09:51:08 +0200

    Add unittest for sorting, update sort to pass unittest

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,4 +1,3 @@
-
 use std::path::{Path, PathBuf};
 use std::fs;
 use crate::events::UpdateEvent;
@@ -170,7 +169,11 @@ impl DirectoryScanner {
     }
 
     fn traverse(&mut self, path: &PathBuf, current_depth: usize) {
-        for entry in std::fs::read_dir(path).unwrap() {
+        let entries = match std::fs::read_dir(path) {
+                Ok(entries) => entries,
+                Err(_) => return,
+            };
+        for entry in entries {
             let entry = entry.unwrap();
             let path = entry.path();
             let name = entry.file_name().to_string_lossy().to_string();
@@ -196,25 +199,41 @@ impl DirectoryScanner {
     }
 
     pub fn sort_entries(&mut self) {
-        self.entries.sort_by(|a, b| {
-        if a.depth != 0 || b.depth != 0 {
-            return std::cmp::Ordering::Equal;
+        let mut groups: Vec<Vec<DirectoryEntry>> = Vec::new();
+        let mut current_group: Vec<DirectoryEntry> = Vec::new();
+        for entry in self.entries.drain(..) {
+            if !current_group.is_empty() && entry.depth == 0 {
+                groups.push(std::mem::take(&mut current_group));
+            }
+            current_group.push(entry);
         }
-        match (&a.size_state, &b.size_state) {
-            (
+        if !current_group.is_empty() {
+            groups.push(std::mem::take(&mut current_group));
+        }
+        groups.sort_by(|a,b|{
+            match (&a[0].size_state, &b[0].size_state) {
+                // Largest size
+                (
                 SizeState::Calculated(a_size),
                 SizeState::Calculated(b_size),
             ) => b_size.cmp(a_size),
 
+            // A size is greater than error or pending
             (SizeState::Calculated(_), _) => std::cmp::Ordering::Less,
             (_, SizeState::Calculated(_)) => std::cmp::Ordering::Greater,
 
+            // Pending is greater than error
             (SizeState::Pending, SizeState::Error) => std::cmp::Ordering::Less,
             (SizeState::Error, SizeState::Pending) => std::cmp::Ordering::Greater,
 
             _ => std::cmp::Ordering::Equal,
-        }
-    });
+            }
+        });
+
+        self.entries = groups
+                        .into_iter()
+                        .flatten()
+                        .collect();
     }
 
    
@@ -382,6 +401,8 @@ mod tests {
 
     mod directory_scanner {
 
+use crate::scanner::SizeState::Calculated;
+
 use super::*;
 
         fn create_empty_directory_scanner() -> DirectoryScanner {
@@ -394,6 +415,21 @@ use super::*;
                 entries: Vec::new()
                 }
         }
+        fn add_entry(directory_scanner: &mut DirectoryScanner,
+            path: PathBuf,
+            size_state: SizeState,
+            depth: usize,
+            kind: EntryType) {
+                directory_scanner
+                .entries
+                .push(
+                    DirectoryEntry {name: "test_name".into(),
+                                path,
+                                depth,
+                                kind,
+                                size_state});
+        }
+
         #[test]
         fn navigate_to_directory() {
             let mut scanner = create_empty_directory_scanner();
@@ -475,6 +511,48 @@ use super::*;
             assert!(!scanner.navigate_to_index(0));
             assert_eq!(scanner.get_current_dir(), &PathBuf::new());
         }
+
+        #[test]
+        fn sort() {
+            let mut scanner = create_empty_directory_scanner();
+
+            let temp = tempfile::tempdir().unwrap();
+
+            let subdir_a = temp.path().join("test1");
+            let subdir_b = temp.path().join("test2");
+            let subdir_c = temp.path().join("test3");
+
+            std::fs::create_dir(&subdir_a).unwrap();
+            std::fs::create_dir(&subdir_b).unwrap();
+            std::fs::create_dir(&subdir_c).unwrap();
+
+            // test1 children
+            let file1 = subdir_a.join("a1.txt");
+            let file2 = subdir_a.join("a2.txt");
+
+            std::fs::write(&file1, vec![0u8; 100]).unwrap();
+            std::fs::write(&file2, vec![0u8; 200]).unwrap();
+
+            // test2 child
+            let file3 = subdir_b.join("b1.txt");
+
+            std::fs::write(&file3, vec![0u8; 500]).unwrap();
+
+            add_entry(&mut scanner, subdir_a.clone(), Calculated(300), 0, EntryType::Directory);
+            add_entry(&mut scanner, file1.clone(), Calculated(200), 1, EntryType::File);
+            add_entry(&mut scanner, file2.clone(), Calculated(100), 1, EntryType::File);
+            add_entry(&mut scanner, subdir_b.clone(), Calculated(500), 0, EntryType::Directory);
+            add_entry(&mut scanner, file3.clone(), Calculated(500), 1, EntryType::File);
+            add_entry(&mut scanner, subdir_c.clone(), Calculated(0), 0, EntryType::Directory);
+            scanner.sort_entries();
+            assert_eq!(scanner.entries[0].path, subdir_b.clone());
+            assert_eq!(scanner.entries[1].path, file3.clone());
+            assert_eq!(scanner.entries[2].path, subdir_a.clone());
+            assert_eq!(scanner.entries[3].path, file1.clone());
+            assert_eq!(scanner.entries[4].path, file2.clone());
+            assert_eq!(scanner.entries[5].path, subdir_c.clone())
+        }
+
 
 
     }
